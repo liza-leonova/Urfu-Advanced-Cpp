88c1655f4db00ebb64bc28aec8827296bee7a80b
liza-leonova <yelizaveta.leonova18@gmail.com>
2024-12-24 00:19:06 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -3,8 +3,10 @@
 #include <memory>
 #include <stdexcept>
 
-enum class FigureType
-{
+// Make PI accessible to tests and other code
+constexpr double PI = 3.14;
+
+enum class FigureType {
     TRIANGLE,
     CIRCLE,
     RECTANGLE,
@@ -12,30 +14,54 @@ enum class FigureType
 
 class Figure {
 public:
+    virtual ~Figure() = default;  // Add virtual destructor for proper cleanup
     virtual FigureType Type() const = 0;
     virtual double Perimeter() const = 0;
     virtual double Area() const = 0;
 };
 
-class Rect : public Figure
-{
+class Rect : public Figure {
+private:
+    double width;
+    double height;
+
+public:
+    Rect(double width, double height);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
-class Triangle : public Figure
-{
+class Triangle : public Figure {
+private:
+    double sideA, sideB, sideC;
+
+public:
+    Triangle(double sideA, double sideB, double sideC);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
-class Circle : public Figure
-{
+class Circle : public Figure {
+private:
+    double radius;
+
+public:
+    Circle(double radius);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
 };
 
-std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
+std::unique_ptr<Figure> make_figure(FigureType type, double paramA, double paramB = 0, double paramC = 0);
 
-class WrongTriangle : public std::invalid_argument
-{
+class WrongTriangle : public std::invalid_argument {
+public:
+    using std::invalid_argument::invalid_argument;
 };
 
-class LessThanZeroParam : public std::invalid_argument
-{
+class LessThanZeroParam : public std::invalid_argument {
+public:
+    using std::invalid_argument::invalid_argument;
 };
-
